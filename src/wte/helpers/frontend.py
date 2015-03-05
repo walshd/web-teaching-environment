23ad6ad45b354550f43054f3762b1db3d25d5c46
Mark <mark.hall@work.room3b.eu>
2015-03-05 15:38:53 +0000

    Created a generic menubar helper function.

--- src/wte/helpers/frontend.py
+++ src/wte/helpers/frontend.py
@@ -156,3 +156,72 @@ def part_summary(part):
             end = content.find('</%s>' % (m.group(1))) + len(m.group(1)) + 3
             return Markup(content[start:end])
     return None
+
+
+def menubar(menu):
+    u"""Generates a menu bar for a nested set of menu groups and menu items. Each menu group is specified as
+    a ``dict`` with the following keys:
+    
+    * *group*: the name of the group
+    * *items*: a list of menu item ``dict``
+    
+    Each menu item is specified as a ``dict``, which has the following keys:
+    
+    * *visible*: whether the element is displayed (``True``/``False``)
+    * *label*: the label text to show
+    * *href*: the href for the menu link
+    * *class*: the CSS class to specify on the menu link [optional]
+    * *icon*: a foundation icon to show for the menu link [optional]
+    * *confirm*: confirmation text to show before the link is followed [optional]
+    * *highlight*: whether to also show the item at the top-level of the menubar (``True``/``False``) [optional]
+    
+    Menu items that are marked as 'highlight: ``True`` and have an 'icon' set, will be shown at the top level of
+    the menubar. All other items are shown in a drop-down menu.
+    
+    :param menu: The list of menu groups and items
+    :type menu: ``list``
+    :return: The generated HTML elements
+    :rtype: :class:`~genshi.builder.tag`"""
+    full_menu = []
+    highlight = []
+    for group in menu:
+        if(len(full_menu) > 0):
+            full_menu.append(tag.li(class_='divider'))
+        for item in group['items']:
+            if item['visible']:
+                full_menu.append(tag.li(tag.a(item['label'],
+                                              href=item['href'],
+                                              class_=item['class'] if 'class' in item else None,
+                                              data_wte_confirm=item['confirm'] if 'confirm' in item else None)))
+                if 'highlight' in item and item['highlight'] and 'icon' in item:
+                    item['group'] = group['group']
+                    highlight.append(item)
+    groups = []
+    group = None
+    for item in highlight:
+        if group != item['group']:
+            groups.append([])
+            group = item['group']
+        groups[len(groups) - 1].append(item)
+    items = []
+    for group in groups:
+        if len(group) == 1:
+            item = group[0]
+            items.append(tag.li(tag.a(tag.span(class_=item['icon']), title=group[0]['label'],
+                                      href=item['href'],
+                                      class_=item['class'] if 'class' in item else None,
+                                      data_wte_confirm=item['confirm'] if 'confirm' in item else None)))
+        else:
+            sub_menu = []
+            for item in group:
+                sub_menu.append(tag.li(tag.a(item['label'],
+                                             href=item['href'],
+                                             class_=item['class'] if 'class' in item else None,
+                                             data_wte_confirm=item['confirm'] if 'confirm' in item else None)))
+            items.append(tag.li(tag.div(tag.a(tag.span(class_=group[0]['icon']), href='#'),
+                                        tag.ul(sub_menu),
+                                        class_='menu')))
+    items.append(tag.li(tag.div(tag.a(tag.span(class_='fi-list'), href='#', title='All actions'),
+                                tag.ul(full_menu),
+                                class_='menu')))
+    return tag.ul(items, class_='menubar')
