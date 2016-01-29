0cdfaa6b697df57783d6b178beb3be0717bc3d50
Mark <mark.hall@work.room3b.eu>
2016-01-29 11:39:36 +0000

    Closed a security hole that allowed access to arbitrary pages for logged-in users.

--- src/wte/models.py
+++ src/wte/models.py
@@ -438,7 +438,7 @@ class Part(Base):
             elif root.has_role(['owner', 'tutor'], user):
                 return True
             elif root.has_role('student', user):
-                if self.status in [u'available', u'archived']:
+                if self.parent.allow(action, user) and self.status in [u'available', u'archived']:
                     return True
             elif self.type == u'module' and self.status == u'available':
                 return True
