8dd1bf3421535622f100087b1981d1169b964c6f
Mark <mark.hall@work.room3b.eu>
2016-06-23 19:14:07 +0100

    Switched version numbering for dev versions to full semantic versioning

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
     ]
 
 setup(name='WebTeachingEnvironment',
-      version='1.2.0',
+      version='1.2.dev0',
       description='The Web Teaching Environment',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
