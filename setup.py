ca417bdea8929331820b2ca27b9229b3e95e27b9
Mark <mark.hall@work.room3b.eu>
2016-07-05 09:28:01 +0100

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requires = [
     ]
 
 setup(name='WebTeachingEnvironment',
-      version='1.2.dev0',
+      version='1.2.0',
       description='The Web Teaching Environment',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
